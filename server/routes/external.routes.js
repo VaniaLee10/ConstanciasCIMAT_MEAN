2a56af68ef1fcf42d32d3d0caed0809a1eb05229
Axel Frederick Félix Jiménez <frederick.felix@outlook.com>
2023-10-05 08:46:07 -0600

    feature/SEP2023/Ext.Formulario.AFFJ.05.10.23

--- server/routes/external.routes.js
+++ server/routes/external.routes.js
@@ -7,6 +7,8 @@ router.get('/', externalCtrl.login);
 router.post('/', externalCtrl.buscarUsuario);
 router.post('/registro', externalCtrl.createUser);
 router.get('/registro', externalCtrl.registro);
+router.get('/formulario', externalCtrl.formulario);
+router.post('/formulario', externalCtrl.createForm);
 
 
 module.exports = router;
